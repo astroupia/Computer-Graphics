28f261024852fe03e324069b84bbe02d4de0677a
Nahom Tegaude <nahomteguade@gmail.com>
2024-05-08 11:46:16 +0300

    Mid-Point Algorithm

--- Circle/cricle.cpp
+++ Circle/cricle.cpp
@@ -1,32 +1,49 @@
-#include <iostream>
 #include <graphics.h>
-#include <stdlib.h>
-#include <dos.h>
-#include <conio.h>
-using namespace std;
 
-int gd = 0, gm;
-int main() {
-    initgraph(&gd, &gm, "..\bgi");
-    initwindow(700,700);
-    int i = 0;
-    int lineY = 0;
-    while (!kbhit()) {
-        setcolor(2);
-        //setlinestyle(0, 0, 3);
-        setfillstyle(3, 2);
-        rectangle(10+i, 10, 40+i, 50);
-        floodfill(15+i, 15, 2);
-        delay(10);
-        setcolor(0);
-        //setlinestyle(0, 0, 3);
-        setfillstyle(3, 0);
-        rectangle(10+i, 10, 40+i, 50);
-        floodfill(15+i, 15, 0);
-        i++;
+void drawCircle(int x_center, int y_center, int radius) {
+    int x = 0, y = radius;
+    int decisionParameter = 1 - radius; // Initial decision parameter
 
+    // Plot the initial point in each octant
+    putpixel(x_center + x, y_center + y, WHITE);
+    putpixel(x_center - x, y_center + y, WHITE);
+    putpixel(x_center + x, y_center - y, WHITE);
+    putpixel(x_center - x, y_center - y, WHITE);
+    putpixel(x_center + y, y_center + x, WHITE);
+    putpixel(x_center - y, y_center + x, WHITE);
+    putpixel(x_center + y, y_center - x, WHITE);
+    putpixel(x_center - y, y_center - x, WHITE);
+
+    // Iterate until the point on the circle crosses the x=y line
+    while (x < y) {
+        x++;
+        if (decisionParameter < 0) {
+            decisionParameter += 2 * x + 1; // Update decision parameter
+        } else {
+            y--;
+            decisionParameter += 2 * (x - y) + 1; // Update decision parameter
+        }
+        // Plot points symmetrically in each octant
+        putpixel(x_center + x, y_center + y, WHITE);
+        putpixel(x_center - x, y_center + y, WHITE);
+        putpixel(x_center + x, y_center - y, WHITE);
+        putpixel(x_center - x, y_center - y, WHITE);
+        putpixel(x_center + y, y_center + x, WHITE);
+        putpixel(x_center - y, y_center + x, WHITE);
+        putpixel(x_center + y, y_center - x, WHITE);
+        putpixel(x_center - y, y_center - x, WHITE);
+        delay(30);
     }
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL); // Initialize graphics mode
+
+    int x_center = 200, y_center = 200, radius = 100;
+    drawCircle(x_center, y_center, radius);
+
     getch();
-    closegraph();
+    closegraph(); // Close graphics mode
     return 0;
 }
